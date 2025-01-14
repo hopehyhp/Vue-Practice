fabcd7739a635fc7f9ce1a03caef901748e5d130
hope <1441499984@qq.com>
2025-01-14 14:30:12 +0800

    Remove my key

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 import CryptoJS from 'crypto-js';
 
 // 加密 API 密钥
-const apiKey = 'f9ba1f64d79dcf68a780e9c4713cc682';
+const apiKey = 'Own key';
 const encryptedApiKey = CryptoJS.AES.encrypt(apiKey, 'your-secret-key').toString();
 
 // 解密函数
@@ -14,4 +14,4 @@ function decrypt(ciphertext) {
 export default {
   apiKey: encryptedApiKey,
   decryptApiKey: decrypt
-}; 
+};
